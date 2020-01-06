bce8842ccadcc9dac9ea08e36c31511a0b2f7bcd
Lars.Petersson <Lars.Petersson@ihsmarkit.com>
2020-01-06 10:35:27 +0000

    small update

--- ThoughtForTheDay.psm1
+++ ThoughtForTheDay.psm1
@@ -1,17 +1,43 @@
-Function Get-ThoughtFortheDay
-{
-	IF (Test-Path "C:\TEMP\Powershell.Modules\ThoughtForTheDay\Thought.For.The.Day.txt")
-		{
-			$TFTDArray = Get-Content "C:\TEMP\Powershell.Modules\ThoughtForTheDay\Thought.For.The.Day.txt"
-		}
-	ELSEIF (Test-Path "$ENV:USERPROFILE\Documents\WindowsPowerShell\Modules\ThoughtForTheDay\Thought.For.The.Day.txt")
-		{
-			$TFTDArray = Get-Content $ENV:USERPROFILE\Documents\WindowsPowerShell\Modules\ThoughtForTheDay\Thought.For.The.Day.txt
-		}
-	ELSE {}
+#region Script Header
+#	Thought for the day:
+#	NAME: ThoughtForTheDay.psm1
+#	AUTHOR: Lars Panzerbjrn
+#	CONTACT: GitHub: Panzerbjrn / Twitter: Panzerbjrn
+#	DATE: 2019.10.01
+#
+#	SYNOPSIS:
+#	Provides a bit of daily wisdom
+#
+#	DESCRIPTION:
+#	Provides a bit of daily wisdom
+#
+#	REQUIREMENTS:
+#
+#endregion Script Header
 
-	$TFTD = Get-Random $TFTDArray
-	Write-Host "Thought for the day:
-	$TFTD
-	"
+#Requires -Version 3.0
+
+[CmdletBinding()]
+param()
+
+Write-Verbose $PSScriptRoot
+
+#Get Functions and Helpers function definition files.
+$Functions	= @( Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue )
+#$Helpers = @( Get-ChildItem -Path $PSScriptRoot\Helpers\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+ForEach ($Import in @($Functions))
+{
+	Try
+	{
+		. $Import.Fullname
+	}
+	Catch
+	{
+		Write-Error -Message "Failed to Import function $($Import.Fullname): $_"
+	}
 }
+
+Export-ModuleMember -Function $Functions.Basename
+
